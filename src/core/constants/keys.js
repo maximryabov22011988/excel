faf3b1bf2af55256227e1f965d2d65e62a725020
Ryabov Maxim <ryabovms@altarix.ru>
2020-11-14 02:12:04 +0400

    Add cell selection

---
+++ src/core/constants/keys.js
@@ -0,0 +1,6 @@
+export const TAB = 'Tab'
+export const ENTER = 'Enter'
+export const ARROW_RIGHT = 'ArrowRight'
+export const ARROW_LEFT = 'ArrowLeft'
+export const ARROW_UP = 'ArrowUp'
+export const ARROW_DOWN = 'ArrowDown'
